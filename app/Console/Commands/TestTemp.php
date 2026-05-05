7fdb7ff84ab8b9b900d831913ac036d931f650aa
Bruno Freire <brunoqm99@gmail.com>
2026-05-05 09:15:54 -0300

    Enhance TestTemp command to display backup temp folder tree and size information

--- app/Console/Commands/TestTemp.php
+++ app/Console/Commands/TestTemp.php
@@ -37,6 +37,12 @@ class TestTemp extends Command
         $this->renderTree($tempFolder);
 
         $this->info('Temp folder size: '.$this->getDirectorySize($tempFolder));
+
+
+        $this->line('Backup temp folder tree:');
+        $this->renderTree(storage_path('app/backup-temp'));
+        $this->info('Location Backup Temp: ' . storage_path('app/backup-temp'));
+        $this->info('Backup temp folder size: '.$this->getDirectorySize(storage_path('app/backup-temp')));
     }
 
     private function renderTree(string $directory, string $prefix = ''): void
